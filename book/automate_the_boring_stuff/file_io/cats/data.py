d067252f00d4df5532c72d6b9c9d6678bf7d2174
Eric Dong <eric2043@gmail.com>
2024-07-23 19:43:34 -0400

    Init sections on file

--- book/automate_the_boring_stuff/file_io/cats/data.py
+++ book/automate_the_boring_stuff/file_io/cats/data.py
@@ -1,7 +1,7 @@
 import json
-from cats import get_asset_path  
+from cats import get_asset_path
 
 def load_data():
-    with open(get_asset_path() / 'data.json', mode="r") as f:
+    with open(get_asset_path() / 'data.json', mode="r", encoding="utf-8") as f:
         return json.load(f)
         
