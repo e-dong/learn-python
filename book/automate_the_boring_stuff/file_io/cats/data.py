f4d14c3e4bb462fbbad7441f678415473acd2a74
Eric Dong <eric2043@gmail.com>
2025-04-16 22:08:52 -0400

    Updated other notebooks

--- book/automate_the_boring_stuff/file_io/cats/data.py
+++ book/automate_the_boring_stuff/file_io/cats/data.py
@@ -3,5 +3,6 @@ from cats import get_asset_path
 
 def load_data():
     with open(get_asset_path() / 'data.json', mode="r", encoding="utf-8") as f:
-        return json.load(f)
+        content = f.read()
+        return json.loads(content)
         
